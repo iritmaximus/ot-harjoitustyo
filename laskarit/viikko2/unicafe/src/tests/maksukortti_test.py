5688614e41e0ca1646fae8fc053e112220ddcbd8
iritmaximus <aaro.koivukangas@gmail.com>
2023-03-29 15:50:41 +0300

    did 2 week but forgot to commit...

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -1,9 +1,13 @@
 import unittest
 from maksukortti import Maksukortti
 
+
 class TestMaksukortti(unittest.TestCase):
     def setUp(self):
         self.maksukortti = Maksukortti(1000)
 
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
+
+    def test_take_too_much_money(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(100000), False)

24edcf2a617f52f46db99e8a23e2643ef6b1d37d
iritmaximus <aaro.koivukangas@gmail.com>
2023-03-31 13:43:08 +0300

    maksukortti tests

--- laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
+++ laskarit/viikko2/unicafe/src/tests/maksukortti_test.py
@@ -9,5 +9,23 @@ class TestMaksukortti(unittest.TestCase):
     def test_luotu_kortti_on_olemassa(self):
         self.assertNotEqual(self.maksukortti, None)
 
-    def test_take_too_much_money(self):
-        self.assertEqual(self.maksukortti.ota_rahaa(100000), False)
+    def test_correct_saldo(self):
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_adding_funds(self):
+        self.maksukortti.lataa_rahaa(100)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 11.00 euroa")
+
+    def test_removing_funds(self):
+        self.maksukortti.ota_rahaa(100)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 9.00 euroa")
+
+    def test_removing_insufficient_funds(self):
+        self.maksukortti.ota_rahaa(11000)
+        self.assertEqual(str(self.maksukortti), "Kortilla on rahaa 10.00 euroa")
+
+    def test_removing_sufficient_funds_return(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(100), True)
+
+    def test_removing_insufficient_funds_return(self):
+        self.assertEqual(self.maksukortti.ota_rahaa(11000), False)
